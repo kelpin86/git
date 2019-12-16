58219119ec2ccb1bc729479aec65d3d47fdfac33
kelpin86 <57132255+kelpin86@users.noreply.github.com>
2019-12-16 18:52:10 +0000

    update githup login

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -3,6 +3,9 @@ package tests;
 public class login {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("I love java");
+        System.out.println("do love java");
+        
 
     }
 }
